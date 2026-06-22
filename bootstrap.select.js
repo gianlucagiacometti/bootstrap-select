200142327fd52566475aa3945906fd341d5a1875
Gianluca Giacometti <52405+gianlucagiacometti@users.noreply.github.com>
2026-06-22 13:22:37 +0200

    Update bootstrap.select.js

--- bootstrap.select.js
+++ bootstrap.select.js
@@ -777,9 +777,10 @@ class bsSelect {
 				let parent
 console.log('insert parent argument', parents[index])
 				if (parents[index]) {
-					if ((typeof parents[index] === "string") && document.querySelector("#" + parents[index])) {
-						let parentElement = document.querySelector("#" + parents[index])
-						if (parentElement.tagName === "OPTGROUP" && parentElement.dataset.rnd) {
+					if (typeof parents[index] === "string") {
+						let parentElement = this.element.querySelector('optgroup[id="' + parents[index] + '"]')
+					
+						if (parentElement && parentElement.dataset.rnd) {
 							parent = parentElement.dataset.rnd
 							list = document.querySelector("#select-option-group-children-" + this.seq + "-" + parent)
 						}
